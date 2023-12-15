3a59a0387b71cce91ad38ae1e109fe16104c7b2f
dixitnishi <dixit.nishi0409@gmail.com>
2023-12-15 18:44:23 +0530

    Solved the to list and file read server

--- week-2/02-nodejs/todoServer.js
+++ week-2/02-nodejs/todoServer.js
@@ -39,11 +39,67 @@
 
   Testing the server - run `npm run test-todoServer` command in terminal
  */
-  const express = require('express');
-  const bodyParser = require('body-parser');
-  
-  const app = express();
-  
-  app.use(bodyParser.json());
-  
-  module.exports = app;
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+
+let todos = [];
+
+app.get("/todos", (req, res) => {
+  res.json(todos);
+});
+
+app.get("/todos/:id", (req,res) => {
+  let todo = todos.find((t) => t.id === parseInt(req.params.id));
+  if (!todo) {
+    res.status(404).send();
+  } else {
+    res.json(todo);
+  }
+});
+
+app.post("/todos", (req, res) => {
+  const newTodo = {
+    id: Math.floor(Math.random() * 1000000),
+    title: req.body.title,
+    description: req.body.description,
+  };
+  todos.push(newTodo);
+  res.status(201).json(newTodo);
+});
+
+app.put("/todos/:id", (req,res) => {
+  const indexOfToDo = todos.findIndex(
+    (toDo) => toDo.id === parseInt(req.params.id)
+  );
+  if (indexOfToDo === -1) {
+    res.status(404).send();
+  } else {
+    todos[indexOfToDo].title = req.body.title;
+    todos[indexOfToDo].description = req.body.description;
+    res.json(todos[indexOfToDo]);
+  }
+});
+
+app.delete("/todos/:id",(req,res)=>{
+  const indexOfToDo = todos.findIndex(
+    (toDo) => toDo.id === parseInt(req.params.id)
+  );
+  if(indexOfToDo === -1){
+    res.status(404).send();
+  }
+  else{
+    todos.splice(indexOfToDo,1);
+    res.status(200).send();
+  }
+})
+
+app.use((req, res, next) => {
+  res.status(404).send();
+});
+
+// app.listen(3000);
+
+module.exports = app;
